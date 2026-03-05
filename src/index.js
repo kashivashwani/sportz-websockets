a469f2be0bf355c7379037ac688036d60b240acd
Mac <mac@Macs-MacBook-Air.local>
2026-03-05 22:57:17 +0530

    implement data seeding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express from 'express';
 import http from 'http';
 import {matchRouter} from "./routes/matches.js";
 import {attachWebSocketServer} from "./ws/server.js";
-import {securityMiddleware} from "./arcjet.js";
 import {commentaryRouter} from "./routes/commentary.js";
 
 const PORT = Number(process.env.PORT || 8000);
@@ -17,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Express server!');
 });
 
-app.use(securityMiddleware());
+// app.use(securityMiddleware());
 
 app.use('/matches', matchRouter);
 app.use('/matches/:id/commentary', commentaryRouter);
