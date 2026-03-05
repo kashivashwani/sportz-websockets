52ad5c1075bdb68b0db883e85696c351dc2c79af
Mac <mac@Macs-MacBook-Air.local>
2026-03-05 21:52:00 +0530

    Implement pub sub broadcasting for commentaries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {matchRouter} from "./routes/matches.js";
 import http from 'http';
+import {matchRouter} from "./routes/matches.js";
 import {attachWebSocketServer} from "./ws/server.js";
 import {securityMiddleware} from "./arcjet.js";
-
+import {commentaryRouter} from "./routes/commentary.js";
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || '0.0.0.0';
@@ -13,20 +13,22 @@ const server = http.createServer(app);
 
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.send('Express server is running.');
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
 });
 
 app.use(securityMiddleware());
 
 app.use('/matches', matchRouter);
+app.use('/matches/:id/commentary', commentaryRouter);
 
-const { broadcastMatchCreated } = attachWebSocketServer(server);
+const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
 
 server.listen(PORT, HOST, () => {
-  const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
 
-  console.log(`Server is running on ${baseUrl}`);
-  console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
 });
